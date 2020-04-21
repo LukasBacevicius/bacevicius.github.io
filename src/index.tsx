1a243be86749e1870079eca795a9fbb016a8ace9
Lukas Bacevicius <lukas.bacevicius@valtech.com>
2020-04-21 21:23:50 +0200

    feat(GA) adds ga

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router'
+import ReactGA from 'react-ga';
 import history from './history';
 import store from './store';
 import App from './App';
+
+ReactGA.initialize('UA-164199624-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 render(
     <Provider store={store}>
